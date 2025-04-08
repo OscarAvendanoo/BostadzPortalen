a673f5bf3f4002652414545cf3175975799be636
Oscar avendano <Oscaravendnaoo@hotmail.com>
2025-04-08 13:50:12 +0200

    Made repository and include methods for PropertyForSale

---
+++ BostadzPortalenWebAPI/Data/PropertyForSaleRepository.cs
@@ -0,0 +1,71 @@
+﻿using BostadzPortalenWebAPI.Models;
+
+namespace BostadzPortalenWebAPI.Data
+{
+    //Author: Oscar och Jonathan
+
+    public class PropertyForSaleRepository : GenericRepository<PropertyForSale>, IPropertyForSaleRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+        public PropertyForSaleRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        // Hämtar alla försäljningsobjekt med full include till en lista
+        public async Task<List<PropertyForSale>> GetAllWithIncludesAsync()
+        {
+            return await _context.PropertyForSale
+                .Include(p => p.Realtor)
+                    .ThenInclude(r => r.RealEastateAgency)
+                .Include(p => p.Municipality)
+                .Include(p => p.Images)
+                .ToListAsync();
+        }
+        // Hämtar ett försäljningsobjekt med hjälp av ID med full include
+        public async Task<PropertyForSale> GetByIDIncludesAsync(int id)
+        {
+
+            return await _context.PropertyForSale
+                .Include(p => p.Realtor)
+                    .ThenInclude(r => r.RealEstateAgency)
+                .Include(p => p.Municipality)
+                .Include(p => p.Images)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+
+        }
+        // Hämtar försäljningsobjekt baserat på MäklarId
+        public async Task<List<PropertyForSale>> GetByRealtorAsync(string realtorId)
+        {
+            return await _context.PropertyForSale
+                .Where(p => p.RealtorId == realtorId)
+                .Include(p => p.Municipality)
+                .Include(p => p.Images)
+                .ToListAsync();
+        }
+
+        // Hämtar alla försäljnings objekt i en viss kommun
+
+        public async Task<List<PropertyForSale>> GetByMunicipalityAsync(int municipalityId)
+        {
+            return await _context.PropertyForSale
+                .Where(p => p.MunicipalityId == municipalityId)
+                .Include(p => p.Images) 
+                .Include(p => p.Realtor)
+                .ToListAsync();
+        }
+
+        // Hämtar alla försäljningsobjekt efter en viss kategori
+        public async Task<List<PropertyForSale>> GetByCategoryAsync(string category)
+        {
+            return await _context.PropertyForSale
+                .Where(p => p.Category == category)
+                .Include(p => p.Realtor)
+                    .ThenInclude(r => r.RealEastateAgency)
+                .Include(p => p.Municipality)
+                .Include(p => p.Images)
+                .ToListAsync();
+        }
+    }
+}
