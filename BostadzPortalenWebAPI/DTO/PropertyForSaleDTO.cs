7300876e94a04bf9a5a5126440303c0f89e7969b
Racke77 <racke.author@outlook.com>
2025-04-08 14:41:18 +0200

    brackets

--- BostadzPortalenWebAPI/DTO/PropertyForSaleDTO.cs
+++ BostadzPortalenWebAPI/DTO/PropertyForSaleDTO.cs
@@ -7,22 +7,22 @@ namespace BostadzPortalenWebAPI.DTO
 {
     public class PropertyForSaleDTO
     {
-
-        public int PropertyForSaleId { get; set; }
-        public string Address { get; set; }
-        public int MunicipalityId { get; set; }
-        public decimal AskingPrice { get; set; }
-        public double LivingArea { get; set; } // kvm
-        public double SupplementaryArea { get; set; } // kvm
-        public double? PlotArea { get; set; } // kvm
-        public string Description { get; set; }
-        public int NumberOfRooms { get; set; }
-        public decimal? MonthlyFee { get; set; }
-        public decimal YearlyOperatingCost { get; set; }
-        public int YearBuilt { get; set; }
-        public List<string> ImageUrls { get; set; } = new List<string>();
-        public string RealtorId { get; set; }
-        public TypeOfPropertyEnum TypeOfProperty { get; set; }
+        
+            public int PropertyForSaleId { get; set; }
+            public string Address { get; set; }
+            public int MunicipalityId { get; set; }
+            public decimal AskingPrice { get; set; }
+            public double LivingArea { get; set; } // kvm
+            public double SupplementaryArea { get; set; } // kvm
+            public double? PlotArea { get; set; } // kvm
+            public string Description { get; set; }
+            public int NumberOfRooms { get; set; }
+            public decimal? MonthlyFee { get; set; }
+            public decimal YearlyOperatingCost { get; set; }
+            public int YearBuilt { get; set; }
+            public List<string> ImageUrls { get; set; } = new List<string>();
+            public string RealtorId { get; set; }
+            public TypeOfPropertyEnum TypeOfProperty { get; set; }
+        }
     }
-}
 
