b411815c01bddc3e62631a9e560b547e330a713f
Kevin Hedendahl <kevin.hed@hotmail.com>
2025-04-17 16:52:20 +0200

    bigpush kalas

---
+++ BostadzPortalenClient/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,39 @@
+﻿
+using Blazored.LocalStorage;
+using BostadzPortalenClient.Services.Base;
+using BostadzPortalenTestApp.Providers;
+using Microsoft.AspNetCore.Components.Authorization;
+
+
+
+namespace BostadzPortalenClient.Services.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IClient httpClient;
+        private readonly ILocalStorageService localStorage;
+        private readonly AuthenticationStateProvider authenticationStateProvider;
+
+        public AuthenticationService(IClient httpClient, ILocalStorageService localStorage, AuthenticationStateProvider authenticationStateProvider)
+        {
+            this.httpClient = httpClient;
+            this.localStorage = localStorage;
+            this.authenticationStateProvider = authenticationStateProvider;
+        }
+        public async Task<bool> AuthenticateAsync(LoginRealtorDto realtorDto)
+        {
+            var response = await httpClient.LoginAsync(realtorDto);
+
+            await localStorage.SetItemAsync("accessToken", response.Token);
+
+            await ((ApiAuthenticationStateProvider)authenticationStateProvider).LoggedIn();
+
+            return true;
+        }
+
+        public Task Logout()
+        {
+            return ((ApiAuthenticationStateProvider)authenticationStateProvider).LoggedOut();
+        }
+    }
+}
