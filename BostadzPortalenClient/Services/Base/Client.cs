6d222a9047731a56454a20f20d0cdca6d1ce33bb
Kevin Hedendahl <kevin.hed@hotmail.com>
2025-04-17 16:07:49 +0200

    big mess

---
+++ BostadzPortalenClient/Services/Base/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace BostadzPortalenClient.Services.Base
+{
+    public partial class Client : IClient
+    {
+        public HttpClient HttpClient
+        {
+            get
+            {
+                return HttpClient;
+            }
+        }
+    }
+}
