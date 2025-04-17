a8581a166da3d111f56424dd011fd73903d063fe
Jonaandersson <jonaandersson1996@gmail.com>
2025-04-17 12:13:27 +0200

    Auth

---
+++ BostadzPortalenWebAPI/DTO/AuthResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BostadzPortalenWebAPI.DTO
+{
+    public class AuthResponseDTO
+    {
+        public string UserId { get; set; }
+        public string Token { get; set; }
+        public string Email { get; set; }
+
+    }
+}
