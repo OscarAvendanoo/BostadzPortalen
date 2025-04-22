3e5b639ca26fd3a6b61aeea083dbc34120f2a43d
Racke77 <racke.author@outlook.com>
2025-04-22 12:50:06 +0200

    basic implementation of GetAll

---
+++ BostadzPortalenClient/Services/PropertyForSaleS/IPropertyForSaleService.cs
@@ -0,0 +1,9 @@
+﻿using BostadzPortalenClient.Services.Base;
+
+namespace BostadzPortalenClient.Services.PropertyForSaleS
+{
+    public interface IPropertyForSaleService
+    {
+        public Task<ICollection<PropertyForSale>> GetAllPropertiesForSaleAsync();
+    }
+}
