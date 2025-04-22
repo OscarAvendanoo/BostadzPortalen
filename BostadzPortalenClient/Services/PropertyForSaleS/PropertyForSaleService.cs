a62d7723df7de53ee4966556748eaea81f84b8ee
Racke77 <racke.author@outlook.com>
2025-04-22 12:50:06 +0200

    basic implementation of GetAll

---
+++ BostadzPortalenClient/Services/PropertyForSaleS/PropertyForSaleService.cs
@@ -0,0 +1,22 @@
+﻿using Blazored.LocalStorage;
+using BostadzPortalenClient.Services.Base;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BostadzPortalenClient.Services.PropertyForSaleS
+{
+    public class PropertyForSaleService: IPropertyForSaleService
+    {
+        private readonly IClient httpClient;
+
+        public PropertyForSaleService(IClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<ICollection<PropertyForSale>> GetAllPropertiesForSaleAsync()
+        {
+            var tests = await httpClient.PropertyForSaleAllAsync();
+            return tests;
+        }
+    }
+}
