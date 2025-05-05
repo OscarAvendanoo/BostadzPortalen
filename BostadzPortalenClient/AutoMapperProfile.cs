566f7e3a12bde64bc70650e4daaed6f2ab07fdc3
Kevin Hedendahl <kevin.hed@hotmail.com>
2025-05-05 07:33:37 +0200

    hmm

--- BostadzPortalenClient/AutoMapperProfile.cs
+++ BostadzPortalenClient/AutoMapperProfile.cs
@@ -9,13 +9,13 @@ namespace BostadzPortalenClient
         {
             CreateMap<PropertyForSale, PropertyForSaleOverviewDTO>()
                 .ForMember(dest => dest.MunicipalityName, opt => opt.MapFrom(src => src.Municipality.Name))
-                .ForMember(dest => dest.ImageUrls, opt => opt.MapFrom(src => src.ImageUrls ?? new List<string>()))
+                .ForMember(dest => dest.ImageUrls, opt => opt.MapFrom(src => src.ImageUrls))
                 .ReverseMap(); 
 
             CreateMap<PropertyForSale, PropertyForSaleDetailsDTO>()
                .ForMember(dest => dest.MunicipalityName, opt => opt.MapFrom(src => src.Municipality.Name))
-               .ForMember(dest => dest.ImageUrls, opt => opt.MapFrom(src => src.ImageUrls ?? new List<string>()))
-               .ForMember(dest => dest.RealEstateAgency, opt => opt.MapFrom(src => src.Realtor.Agency))
+               .ForMember(dest => dest.ImageUrls, opt => opt.MapFrom(src => src.ImageUrls))
+               //.ForMember(dest => dest.Realtor, opt => opt.MapFrom(src => src.Realtor.Agency))
                .ReverseMap();
         }
     }
