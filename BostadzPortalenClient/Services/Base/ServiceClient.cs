e351369f6c38c060e250127067b520a496cb293e
Oscar avendano <Oscaravendnaoo@hotmail.com>
2025-05-10 00:52:10 +0200

    Made a new nswag scafold

--- BostadzPortalenClient/Services/Base/ServiceClient.cs
+++ BostadzPortalenClient/Services/Base/ServiceClient.cs
@@ -3044,9 +3044,7 @@ namespace BostadzPortalenClient.Services.Base
 
     }
 
-
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.2.0.0 (NJsonSchema v11.1.0.0 (Newtonsoft.Json v13.0.0.0))")]
-
     public partial class LoginRealtorDto
     {
         [Newtonsoft.Json.JsonProperty("email", Required = Newtonsoft.Json.Required.Always)]
