07def24671dcf7c87c3fb76501f7558ce7e876d5
Racke77 <racke.author@outlook.com>
2025-05-09 20:51:57 +0200

    1-admin, 12-realtors, 7-agencies (1 for only admin),  11-municipalities, 20-properties (3 pics each) -> need more props still

--- BostadzPortalenWebAPI/Constants/SeedGUID.cs
+++ BostadzPortalenWebAPI/Constants/SeedGUID.cs
@@ -10,5 +10,27 @@
         public static string RoleUser = "e6ae5d04-8f49-4b62-bc52-205353eb08dc";
         public static string RoleAdmin = "e11cc563-5369-471e-9792-6255e0d08eaf";
         public static string RoleRealtor = "1e346bcf-ee97-4bb2-ab3e-8d7202fea078";
+
+        public static string Realtor1 = "26f1c93b-0ce3-4655-a0e1-f0d91dbe6c71";
+        public static string Realtor2 = "a64ba80d-139c-4816-a169-9344e3d58e22";
+        public static string Realtor3 = "b03b6b66-a5fd-4fbc-a3f2-9c7202cbee60";
+        public static string Realtor4 = "91858c77-9f96-4e67-8b4d-12065003247a";
+        public static string Realtor5 = "e7cde164-d981-41df-b3d9-35d42c0d8323";
+        public static string Realtor6 = "a7158f9e-8afb-4389-8e68-348450f4b6a9";
+        public static string Realtor7 = "144f56f5-10d1-4ab3-ab72-e917ec9cacc4";
+        public static string Realtor8 = "66787f78-4dd5-4c82-917e-1bf99028a59a";
+        public static string Realtor9 = "36cd14e4-aca3-4108-8120-42b5ffaee35f";
+        public static string Realtor10 = "0a0ee7be-cde2-49bd-b48d-b675a4fbfbf0";
+        //public static string Realtor11 = "82723383-04e8-472e-8200-b44c2fcc4c0a";
+        //public static string Realtor12 = "0b6f3107-b234-4115-992e-68005b4faa7c";
+        //public static string Realtor13 = "e62b1c57-2c76-4395-a2f6-adb66e73b001";
+        //public static string Realtor14 = "afe8428b-7c1b-4897-a10d-219c587b932d";
+        //public static string Realtor15 = "4f902f32-92d5-476a-a0b3-a6293d7195ba";
+        //public static string Realtor16 = "d895b75f-cd96-4765-9728-b90464b12ea1";
+        //public static string Realtor17 = "069a5d3d-7642-4392-afc1-791d48589575";
+        //public static string Realtor18 = "79723194-3181-47ad-a5b4-c122acfe4dd7";
+        //public static string Realtor19 = "a72f2b26-9a5c-4183-82ff-fdde904f4a8f";
+        //public static string Realtor20 = "4b37f594-ca28-423c-97d2-4939ed3363d3";
+
     }
 }
