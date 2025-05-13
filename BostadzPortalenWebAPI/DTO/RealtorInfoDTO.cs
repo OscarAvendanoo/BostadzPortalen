aeb8a89f108e38bda4355e87c18ef4d6fb2f4798
Racke77 <racke.author@outlook.com>
2025-05-13 10:37:19 +0200

    some comments about things + email/phone icons

--- BostadzPortalenWebAPI/DTO/RealtorInfoDTO.cs
+++ BostadzPortalenWebAPI/DTO/RealtorInfoDTO.cs
@@ -10,7 +10,12 @@
         public string Email { get; set; } = "";
         public string AgencyName { get; set; } = "";
         public string RealtorImage { get; set; } = "";
+        //Needs to include the Agency-Logo
+        //public string AgencyLogo {get; set;} = "";
+
+        //Needs to be Lists, not IEnumerable
         public IEnumerable<PropertyImageDto> PropertyImages { get; set; }
         public IEnumerable<PropertyForSaleOverviewDTO>? Properties { get; set; }
+
     }
 }
