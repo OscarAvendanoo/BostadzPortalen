c10c7a31d6e4bef7b4da861545f64d9150deb885
Kevin Hedendahl <kevin.hed@hotmail.com>
2025-05-19 08:35:18 +0200

    fix js och register pages

--- BostadzPortalenClient/wwwroot/searchbox.js
+++ BostadzPortalenClient/wwwroot/searchbox.js
@@ -1,20 +1,20 @@
 ﻿/*console.log("js startar")*/
-window.setupSearchBoxTabs = () => {
-    document.querySelectorAll('.searchbox-tabs .nav-link').forEach(tab => {
-        tab.addEventListener('click', function (event) {
-            event.preventDefault();
-            const targetTab = this.getAttribute('data-tab');
+//window.setupSearchBoxTabs = () => {
+//    document.querySelectorAll('.searchbox-tabs .nav-link').forEach(tab => {
+//        tab.addEventListener('click', function (event) {
+//            event.preventDefault();
+//            const targetTab = this.getAttribute('data-tab');
 
-            document.querySelectorAll('.tab-content').forEach(content => {
-                content.style.display = 'none';
-            });
+//            document.querySelectorAll('.tab-content').forEach(content => {
+//                content.style.display = 'none';
+//            });
 
-            document.getElementById(targetTab).style.display = 'block';
+//            document.getElementById(targetTab).style.display = 'block';
 
-            document.querySelectorAll('.searchbox-tabs .nav-link').forEach(link => {
-                link.classList.remove('active');
-            });
-            this.classList.add('active');
-        });
-    });
-};
+//            document.querySelectorAll('.searchbox-tabs .nav-link').forEach(link => {
+//                link.classList.remove('active');
+//            });
+//            this.classList.add('active');
+//        });
+//    });
+//};
